f795acaa27be6f07851d1adc589f86506a3143b7
Kacper <kacperglazer73@gmail.com>
2021-10-05 22:44:02 +0200

    pierwszy taks

---
+++ task_1.py
@@ -0,0 +1,32 @@
+# firstly need to know what is difference between signed and unsigned intigers
+# unsigned is form <0;4294967295>
+# signed is from <-2147483648;2147483647>
+
+# function increase number by 2147483648, so if we give -2147483648 it's equal to 0
+def Unsigned_integer_to_signed_integer(intiger):
+    return intiger+2**31
+
+#function check digit if it is 32-bits int it return it back, otherwise return 0
+def is_Unsigned_int32(intiger):
+    if not(Unsigned_integer_to_signed_integer(intiger) >> 32):
+        return intiger
+    else:
+        return 0
+
+#i used reverse of strings to reverse the digit,
+# if it's negative digit it took "-" reverse string (number) without sign (which is '-') and glue these two values together at the end
+#else it just reverse
+#at the end return int value
+def reverse_digit_32_bit(val):
+    x = str(is_Unsigned_int32(val))
+    if x[0] == '-':
+        a = x[::-1]
+        rev=x[0]+a[:-1]
+    else:
+        rev=x[::-1]
+
+    return print(int(rev))
+
+if __name__ == "__main__":
+    val = int(input())
+    reverse_digit_32_bit(val)
